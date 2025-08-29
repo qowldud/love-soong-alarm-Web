19c697df10ac07b1a0f13c1a93119dae0e4ecab0
qowldud <bjy6631@daum.net>
2025-08-30 02:49:57 +0900

    feat: Header 공통 컴포넌트 구현

---
+++ src/common/Header.tsx
@@ -0,0 +1,17 @@
+import Arrow_Back from "@/assets/icons/arrow_back.svg?url";
+
+interface Props {
+  title: string;
+}
+
+export const Header = ({ title }: Props) => {
+  return (
+    <div className="w-full p-4">
+      <div className="flex justify-between items-center">
+        <img src={Arrow_Back} alt="arrow_back" className="size-6" />
+        <span className="text-lg font-medium text-base]">{title}</span>
+        <span className="size-6"></span>
+      </div>
+    </div>
+  );
+};
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,9 +1,11 @@
 import { Button } from "../common/Button";
+import { Header } from "../common/Header";
 
 export const Test = () => {
   return (
     <div>
-      <div className="flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <Header title="프로필 수정" />
         <Button variant="secondary">건너뛰기</Button>
       </div>
     </div>
